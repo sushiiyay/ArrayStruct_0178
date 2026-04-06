1a7c37bded2c0dcfeb97d1534944186fd3d0371e
sushiiyay <nfa.aliya7811@gmail.com>
2026-04-06 15:11:44 +0700

    menambahkan data pertama

--- arrayNew.cpp
+++ arrayNew.cpp
@@ -14,4 +14,17 @@ int main(){
     cout << endl;
     cout << "Data pada index 1 = " << aku[1] << endl;
     // Mengisi Satu per Satu
+    cout << "Data index 0 = ";
+    cin >> kamu[0];
+    cout << "Data index 1 = ";
+    cin >> kamu[1];
+    cout << "Data index 2 = ";
+    cin >> kamu[2];
+    cout << "Data index 3 = ";
+    cin >> kamu[3];
+    cout << "Data index 4 = ";
+    cin >> kamu[4];
+    cout << endl;
+    // Menampilkan Satu per Satu
+    cout << "Data Pertama = " << kamu[0] << endl;
 }
